40b73de70dcdc7321df8e11bfce5922ae9d260f8
audh25-lab <audh25@gmail.com>
2025-12-18 00:42:54 +0500

    Update DayNightSystem.ts

--- src/systems/DayNightSystem.ts
+++ src/systems/DayNightSystem.ts
@@ -3,6 +3,9 @@ import Phaser from 'phaser';
 export default class DayNightSystem {
   public isNight = false;
   constructor(scene: Phaser.Scene) {
-    scene.time.addEvent({ delay: 300000, callback: () => this.isNight = !this.isNight, loop: true });
+    scene.time.addEvent({ delay: 300000, callback: () => {
+      this.isNight = !this.isNight;
+      scene.sound.get('ambient_city').volume = this.isNight ? 0.3 : 0.5;
+    }, loop: true });
   }
 }
