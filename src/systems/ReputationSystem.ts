63cf22da28f3cb500ed2f6341125383ccd25c744
audh25-lab <audh25@gmail.com>
2025-12-18 00:42:28 +0500

    Update ReputationSystem.ts

--- src/systems/ReputationSystem.ts
+++ src/systems/ReputationSystem.ts
@@ -2,4 +2,5 @@ export default class ReputationSystem {
   private rep: number = 0;
   accumulate(delta: number) { this.rep += delta; }
   get() { return this.rep; }
+  load(rep: number) { this.rep = rep; }
 }
